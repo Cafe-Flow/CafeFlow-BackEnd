275c03a94176dbf318e18222caa25d06f4f40d4b
비니빈니 <phb4101@gmail.com>
2024-03-22 20:04:47 +0900

    [INITIAL] 프로젝트 생성
    [INITIAL] 프로젝트 생성

---
+++ src/main/java/org/example/cafeflow/CafeFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.cafeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cafeflow/CafeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cafeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
